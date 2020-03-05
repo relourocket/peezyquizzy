fd91b8aaca432d79568ebb8cd28ba371f97400c9
aurelie <41026494+relourocket@users.noreply.github.com>
2020-03-05 18:10:53 +0100

    inscription + connexion + themes

---
+++ includes/functions_db.php
@@ -0,0 +1,90 @@
+<?php
+
+function connect_db () {
+    $mysqli = new mysqli("localhost", "root", "", "myquizz");
+    // Check connection
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+    else {
+        return $mysqli;
+    }
+}
+
+function check_logs($login, $password) {
+    $sql = connect_db();
+    if ($sql != null) {
+        // prepare and bind
+        $stmt = $sql->prepare("SELECT * FROM users WHERE user_login = ? AND user_password = ?");
+        $stmt->bind_param("ss", $id, $pass);
+        $id = $login;
+        $pass = $password;
+        $stmt->execute();
+        $result = $stmt->get_result(); // get the mysqli result
+        $logs = $result->fetch_all(); // fetch data
+        $stmt->close();
+
+        if (!empty($logs)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    } else {
+        die("No database found...");
+    }
+}
+
+function pseudo_exists ($login) {
+    $sql = connect_db();
+    if ($sql != null) {
+        // prepare and bind
+        $stmt = $sql->prepare("SELECT * FROM users WHERE user_login = ?");
+        $stmt->bind_param("s", $id);
+        $id = $login;
+        $stmt->execute();
+        $result = $stmt->get_result(); // get the mysqli result
+        $logs = $result->fetch_all(); // fetch data
+        $stmt->close();
+
+        if (!empty($logs)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    } else {
+        die("No database found...");
+    }
+}
+
+function signin ($login, $password) {
+    $sql = connect_db();
+    if ($sql != null) {
+        $stmt = $sql->prepare("INSERT INTO users VALUES (0, ?, ?, 0)");
+        $stmt->bind_param("ss", $id, $pswd);
+        $id = $login;
+        $pswd = $password;
+        $stmt->execute();
+        $stmt->close();
+    } else {
+        die("No database found...");
+    }
+}
+
+function get_all_themes () {
+    $sql = connect_db();
+    if ($sql != null) {
+        // prepare and bind
+        $stmt = $sql->prepare("SELECT * FROM theme");
+        $stmt->execute();
+        $result = $stmt->get_result(); // get the mysqli result
+        $themes = $result->fetch_all(); // fetch data
+        $stmt->close();
+
+        return $themes;
+
+    } else {
+        die("No database found...");
+    }
+}
--- views/choixTheme.php
+++ views/choixTheme.php
@@ -2,28 +2,40 @@
 
 <html>
 
-    <head>
-        <meta charset="utf-8"/>
-
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/style.css">
-
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-    </head>
+    <?php include "../includes/head.php";
+          require_once "../includes/functions_db.php";
+    ?>
 
     <body>
-        <?php include("../includes/navbar.php"); ?>
+        <?php
+        require_once "../includes/functions_db.php";
+        if (isset($_POST['login']) && isset($_POST['mdp'])) {
+            if (!pseudo_exists($_POST['login'])) {
+                echo "Pseudo existe pas";
+                header('location: ./connexion.php?error=2');
+            }
+            else if(!check_logs($_POST['login'], $_POST['mdp'])) {
+                header('location: ./connexion.php?error=1');
+            }
+        }
+
+        ?>
+        <?php include("../includes/navbar.php");
+              $themes = get_all_themes();
+        ?>
 
         <div class="choixConteneur">
-            <?php include("../includes/theme.php"); ?>
-            <?php include("../includes/theme.php"); ?>
-            <?php include("../includes/theme.php"); ?>
-            <?php include("../includes/theme.php"); ?>
-            <?php include("../includes/theme.php"); ?>
 
+            <?php
+            $i = 0;
+            foreach ($themes as $key => $value) {
+                echo "<a class = 'themeButton' href='#'>" . utf8_encode($themes[$i][1]) .
+                    "<br>" . utf8_encode($themes[$i][2]) .
+                    "<br><img src = '.." . utf8_encode($themes[$i][3]) . "'/>" .
+                    "</a>";
+                $i++;
+            }
+            ?>
 
         </div>
 
