ee46f0d55d7b6920d970793f62cc507cc632d4dc
Antoine PARIZE <ant.parize@gmail.com>
2020-03-08 19:26:44 +0100

    ajout session + début navbar adaptative

---
+++ includes/navbarDropdown.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Utilisateur
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="#">Profil</a>
+            <a class="dropdown-item" href="#">Se déconnecter</a>
+        </div>
+    </body>
+</html>
