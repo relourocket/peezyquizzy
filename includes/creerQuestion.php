eb5e28b5cdf74107f18793369975ac42feca9243
aurelie <41026494+relourocket@users.noreply.github.com>
2020-03-17 10:00:50 +0100

    insertion BDD pour qcm

--- includes/creerQuestion.php
+++ includes/creerQuestion.php
@@ -22,8 +22,38 @@
                   isset($_POST['rep6'])  && isset($_POST['rep7']) &&
                   isset($_POST['rep8'])) {
 
-            save_question($_POST['enonce'], $_POST['type_question'], $_POST['rep'], $_POST['rep1'], $_POST['rep2'], $_POST['rep3'], $_POST['rep4'],
-                          $_POST['rep5'], $_POST['rep6'], $_POST['rep7'], $_POST['rep8']);
+            if (isset($_POST['rep1T'])) {
+                save_question($_POST['enonce'], $_POST['type_question'], $_POST['rep'], $_POST['rep1'], $_POST['rep2'], $_POST['rep3'], $_POST['rep4'],
+                    $_POST['rep5'], $_POST['rep6'], $_POST['rep7'], $_POST['rep8'], 1);
+            }
+            elseif (isset($_POST['rep2T'])) {
+                save_question($_POST['enonce'], $_POST['type_question'], $_POST['rep'], $_POST['rep1'], $_POST['rep2'], $_POST['rep3'], $_POST['rep4'],
+                    $_POST['rep5'], $_POST['rep6'], $_POST['rep7'], $_POST['rep8'], 2);
+            }
+            elseif (isset($_POST['rep3T'])) {
+                save_question($_POST['enonce'], $_POST['type_question'], $_POST['rep'], $_POST['rep1'], $_POST['rep2'], $_POST['rep3'], $_POST['rep4'],
+                    $_POST['rep5'], $_POST['rep6'], $_POST['rep7'], $_POST['rep8'], 3);
+            }
+            elseif (isset($_POST['rep4T'])) {
+                save_question($_POST['enonce'], $_POST['type_question'], $_POST['rep'], $_POST['rep1'], $_POST['rep2'], $_POST['rep3'], $_POST['rep4'],
+                    $_POST['rep5'], $_POST['rep6'], $_POST['rep7'], $_POST['rep8'], 4);
+            }
+            elseif ($_POST['rep5T']) {
+                save_question($_POST['enonce'], $_POST['type_question'], $_POST['rep'], $_POST['rep1'], $_POST['rep2'], $_POST['rep3'], $_POST['rep4'],
+                    $_POST['rep5'], $_POST['rep6'], $_POST['rep7'], $_POST['rep8'], 5);
+            }
+            elseif (isset($_POST['rep6T'])) {
+                save_question($_POST['enonce'], $_POST['type_question'], $_POST['rep'], $_POST['rep1'], $_POST['rep2'], $_POST['rep3'], $_POST['rep4'],
+                    $_POST['rep5'], $_POST['rep6'], $_POST['rep7'], $_POST['rep8'], 6);
+            }
+            elseif (isset($_POST['rep7T'])) {
+                save_question($_POST['enonce'], $_POST['type_question'], $_POST['rep'], $_POST['rep1'], $_POST['rep2'], $_POST['rep3'], $_POST['rep4'],
+                    $_POST['rep5'], $_POST['rep6'], $_POST['rep7'], $_POST['rep8'], 7);
+            }
+            elseif (isset($_POST['rep8T'])) {
+                save_question($_POST['enonce'], $_POST['type_question'], $_POST['rep'], $_POST['rep1'], $_POST['rep2'], $_POST['rep3'], $_POST['rep4'],
+                    $_POST['rep5'], $_POST['rep6'], $_POST['rep7'], $_POST['rep8'], 8);
+            }
 
         }
 
@@ -64,11 +94,19 @@
                         <div class="col-sm-8">
                           <input type="text" name="rep1" class="form-control" id="rep">
                         </div>
+                        <div class="col-sm-8 goodAnswer">
+                            <input type="radio" name="rep1T">
+                            <label for="rep1T">Réponse juste</label>
+                        </div>
                     </div>
                     <div class="form-group row">
                         <label for="rep2" class="col-sm-4 col-form-label">Réponse 2</label>
                         <div class="col-sm-8">
                           <input type="text" name="rep2" class="form-control" id="rep">
+                        </div>
+                        <div class="col-sm-8 goodAnswer">
+                            <input type="radio" name="rep2T">
+                            <label for="rep2T">Réponse juste</label>
                         </div>
                     </div>
                     <div class="form-group row">
@@ -76,11 +114,19 @@
                         <div class="col-sm-8">
                             <input type="text" name="rep3" class="form-control" id="rep">
                         </div>
+                        <div class="col-sm-8 goodAnswer">
+                            <input type="radio" name="rep3T">
+                            <label for="rep3T">Réponse juste</label>
+                        </div>
                     </div>
                     <div class="form-group row">
                         <label for="rep4" class="col-sm-4 col-form-label">Réponse 4</label>
                         <div class="col-sm-8">
                             <input type="text" name="rep4" class="form-control" id="rep">
+                        </div>
+                        <div class="col-sm-8 goodAnswer">
+                            <input type="radio" name="rep4T">
+                            <label for="rep4T">Réponse juste</label>
                         </div>
                     </div>
                     <div class="form-group row">
@@ -88,11 +134,19 @@
                         <div class="col-sm-8">
                             <input type="text" name="rep5" class="form-control" id="rep">
                         </div>
+                        <div class="col-sm-8 goodAnswer">
+                            <input type="radio" name="rep5T">
+                            <label for="rep5T">Réponse juste</label>
+                        </div>
                     </div>
                     <div class="form-group row">
                         <label for="rep6" class="col-sm-4 col-form-label">Réponse 6</label>
                         <div class="col-sm-8">
                             <input type="text" name="rep6" class="form-control" id="rep">
+                        </div>
+                        <div class="col-sm-8 goodAnswer">
+                            <input type="radio" name="rep6T">
+                            <label for="rep6T">Réponse juste</label>
                         </div>
                     </div>
                     <div class="form-group row">
@@ -100,11 +154,19 @@
                         <div class="col-sm-8">
                             <input type="text" name="rep7" class="form-control" id="rep">
                         </div>
+                        <div class="col-sm-8 goodAnswer">
+                            <input type="radio" name="rep7T">
+                            <label for="rep7T">Réponse juste</label>
+                        </div>
                     </div>
                     <div class="form-group row">
                         <label for="rep8" class="col-sm-4 col-form-label">Réponse 8</label>
                         <div class="col-sm-8">
                             <input type="text" name="rep8" class="form-control" id="rep">
+                        </div>
+                        <div class="col-sm-8 goodAnswer">
+                            <input type="radio" name="rep8T">
+                            <label for="rep8T">Réponse juste</label>
                         </div>
                     </div>
                 </div>
