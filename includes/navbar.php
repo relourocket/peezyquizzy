32c53d8c73659b226da7c7b2da9013b6f53bac3f
relourocket <aurelie.gass@gmail.com>
2020-04-08 13:35:28 +0100

    fix responsive

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,34 +2,63 @@
 
 <html>
 
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-
     <body>
-        <nav class="navbar navbar-expand navbar-light bg-light">
+        <!-- <nav class="navbar navbar-expand navbar-light bg-light">
             <div class="flex_nav">
                 <div id="navigation">
                     <a class="navbar-brand" href="choixTheme.php">QueezyPeezy</a>
 
                     <?php
-                        if(isset($_SESSION["isAdmin"]) && $_SESSION["isAdmin"]==true){
+                       /* if(isset($_SESSION["isAdmin"]) && $_SESSION["isAdmin"]==true){
                             include "../includes/navbarGererJouer.php";
-                        }
+                        }*/
                     ?>
 
                 </div>
 
                 <div class="dropdown navbar-brand">
-                    <?php
+                    <?php /*
                         if(isset($_SESSION["login"])){
                             include("../includes/navbarDropdown.php") ;
-                        }
+                        }*/
                     ?>
                 </div>
             </div>
+        </nav> -->
 
-        </nav>
+        <div class="navbar navbar-expand navbar-light bg-light">
+            <div class="navbar-inner flex_nav">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+
+                    <div class="nav-collapse">
+                        <ul class="nav">
+                            <li><a class="navbar-brand" href="choixTheme.php">QueezyPeezy</li>
+                            <li>
+                                <?php
+                                 if(isset($_SESSION["isAdmin"]) && $_SESSION["isAdmin"]==true){
+                                     include "../includes/navbarGererJouer.php";
+                                 }
+                                ?>
+                            </li>
+                            <li>
+                                <div class="dropdown navbar-brand">
+                                    <?php
+                                        if(isset($_SESSION["login"])){
+                                            include("../includes/navbarDropdown.php") ;
+                                        }
+                                    ?>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.navbar -->
 
     </body>
 
